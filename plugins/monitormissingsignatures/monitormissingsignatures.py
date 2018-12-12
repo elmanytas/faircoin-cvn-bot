f2cec8a56abd9a5fce9764aa6691d41b7524c88d
Leo Arias <yo@elopio.net>
2018-12-12 22:52:23 +0000

    Report after 5 signatures missing

--- plugins/monitormissingsignatures/monitormissingsignatures.py
+++ plugins/monitormissingsignatures/monitormissingsignatures.py
@@ -6,7 +6,7 @@ from bitcoinrpc import authproxy
 
 
 _GROUP_FAIRCOIN_CVN_OPERATORS= '-114923528'
-_POLL_FREQUENCY = 60  # 1 minute.
+_POLL_FREQUENCY = 180  # 3 minutes.
 _FAIRCOIN_RPC_URL = 'http://{}:{}@127.0.0.1:8332'
 _CVN_OPERATORS = {
     '0x0b4e533d': '@piki46',
@@ -32,22 +32,26 @@ class MonitorMissingSignatures(errbot.BotPlugin):
     def activate(self):
         super().activate()
         self['latest_block_hash'] = 0
+        self['missing_count'] = {}
         self.start_poller(_POLL_FREQUENCY, self._report_missing_signatures)
 
     def _report_missing_signatures(self):
         latest_block_hash = self._get_latest_block_hash()
         if latest_block_hash != self['latest_block_hash']:
-            group = self.build_identifier(_GROUP_FAIRCOIN_CVN_OPERATORS)
             self['latest_block_hash'] = latest_block_hash
+            group = self.build_identifier(_GROUP_FAIRCOIN_CVN_OPERATORS)
+
             missing_signatures = self._get_missing_signatures(
                 latest_block_hash)
-            if missing_signatures:
-                missing_operators = ','.join([
-                    _CVN_OPERATORS.get(signature, signature)
-                    for signature in missing_signatures])
-                message = 'Block hash {} missing signatures from {}'.format(
-                    latest_block_hash, missing_operators)
-                return self.send(group, message)
+            self._update_missing_count(missing_signatures)
+            signatures_to_report = self._get_signatures_to_report()
+            if signatures_to_report:
+                for signature in signatures_to_report:
+                    message = '{}: your node is down.'.format(
+                        CVN_OPERATORS.get(signature, signature))
+                    return self.send(group, message)
+            else:
+                self['missing_count'] = {}
 
     def _get_latest_block_hash(self):
         rpc_connection = self._get_rpc_connection()
@@ -55,7 +59,12 @@ class MonitorMissingSignatures(errbot.BotPlugin):
 
     def _get_missing_signatures(self, block_hash):
         rpc_connection = self._get_rpc_connection()
-        return rpc_connection.getblock(block_hash)['missingCreatorIds']
+        missing_signatures = rpc_connection.getblock(
+            block_hash)['missingCreatorIds']
+        if missing_signatures:
+            self.log.info('Block {} is missing signatures from {}'.format(
+                block_hash, missing_signatures))
+        return missing_signatures
 
     def _get_rpc_connection(self):
         # XXX it seems that the connection is not kept alive.
@@ -63,3 +72,20 @@ class MonitorMissingSignatures(errbot.BotPlugin):
             _FAIRCOIN_RPC_URL.format(
                 os.environ.get('FAIRCOIN_RPC_USER'),
                 os.environ.get('FAIRCOIN_RPC_PASSWORD')))
+
+    def _update_missing_count(self, missing_signatures):
+        # Reset the count for signatures that are no longer missing.
+        for signature in self['missing_count']:
+            if signature not in missing_signatures:
+                del self['missing_count'][signature]
+
+        for signature in missing_signatures:
+            count = self['missing_count'].get(signature, 0)
+            self['missing_count'][signature] = count + 1
+
+    def _get_signatures_to_report(self):
+        signatures_to_report = []
+        for signature in self['missing_count']:
+            # 5 missing signatures in a row.
+            if self['missing_count'][signature] == 5:
+                signatures_to_report.append(signature)
