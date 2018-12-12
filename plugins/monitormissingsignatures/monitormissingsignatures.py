d565aa5c0f47301950fb4bbd082d60ed89cce7ba
Ubuntu <ubuntu@faircoin.lxd>
2018-12-12 21:17:21 +0000

    Add missing parenthesis

--- plugins/monitormissingsignatures/monitormissingsignatures.py
+++ plugins/monitormissingsignatures/monitormissingsignatures.py
@@ -9,9 +9,9 @@ class MonitorMissingSignatures(BotPlugin):
         self.rpc_connection = authproxy.AuthServiceProxy(
             'http://{}:{}@127.0.0.1:8332'.format(
                 os.environ.get('FAIRCOIN_RPC_USER'),
-                os.environ.get('FAIRCOIN_RPC_PASSWORD'))
+                os.environ.get('FAIRCOIN_RPC_PASSWORD')))
         self.start_poller(60, self.check_latest_block)
 
     def check_latest_block(self):
-        user =  self.build_identifier('43624396')
+        user = self.build_identifier('43624396')
         return self.send(user, self.rpc_connection.getbestblockhash())
