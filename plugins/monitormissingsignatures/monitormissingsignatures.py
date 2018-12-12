2205ff09a33917b0c6286e3e679296f50b4aaafc
Leo Arias <yo@elopio.net>
2018-12-12 21:57:12 +0000

    Return the missing signatures

--- plugins/monitormissingsignatures/monitormissingsignatures.py
+++ plugins/monitormissingsignatures/monitormissingsignatures.py
@@ -18,13 +18,23 @@ class MonitorMissingSignatures(errbot.BotPlugin):
         self.start_poller(_POLL_FREQUENCY, self._check_latest_block)
 
     def _check_latest_block(self):
-        user = self.build_identifier(_GROUP_FAIRCOIN_CVN_OPERATORS)
-        self['latest_block'] = self._get_latest_block()
-        return self.send(user, self['latest_block'])
+        latest_block_hash = self._get_latest_block_hash()
+        if latest_block_hash != self['latest_block_hash']:
+            group = self.build_identifier(_GROUP_FAIRCOIN_CVN_OPERATORS)
+            self['latest_block_hash'] = latest_block_hash
+            missing_signatures = self._get_missing_signatures(
+                latest_block_hash)
+            message = 'Block hash {} missing signatures {}'.format(
+                latest_block_hash, missing_signatures)
+            return self.send(group, message)
 
     def _get_latest_block(self):
         rpc_connection = self._get_rpc_connection()
         return rpc_connection.getbestblockhash()
+
+    def _get_missing_signatures(self, block_hash):
+        rpc_connection = self._get_rpc_connection()
+        return rpc_connection.getblock(block_hash)['missingCreatorIds']
 
     def _get_rpc_connection(self):
         # XXX it seems that the connection is not kept alive.
